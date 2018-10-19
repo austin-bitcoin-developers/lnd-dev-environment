b127edd275d2db67394b81f1a9ae80dce4f631a0
Richard Bondi <r_bondi@hotmail.com>
2018-10-19 12:54:45 -0500

    lnd version

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
-alias ln-alice="lncli --rpcserver=localhost:10001 --macaroonpath=alice/data/admin.macaroon"
-alias ln-bob="lncli --rpcserver=localhost:10002 --macaroonpath=bob/data/admin.macaroon"
-alias ln-charlie="lncli --rpcserver=localhost:10003 --macaroonpath=charlie/data/admin.macaroon"
+# lnd v4 macaroonpath=[alice|bob|charlie]/data/admin.macaroon
+alias ln-alice="lncli --rpcserver=localhost:10001 --macaroonpath=alice/data/chain/bitcoin/simnet/admin.macaroon"
+alias ln-bob="lncli --rpcserver=localhost:10002 --macaroonpath=bob/data/chain/bitcoin/simnet/admin.macaroon"
+alias ln-charlie="lncli --rpcserver=localhost:10003 --macaroonpath=charlie/data/chain/bitcoin/simnet/admin.macaroon"
